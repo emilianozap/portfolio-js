02fa5adb012c7307fc28c4a328cd96063f89332c
emiliano zapata <emilianozapatamarin@hotmail.com>
2023-10-06 15:24:19 -0300

    animaciones

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,39 @@
+const nav = document.querySelector("#nav")
+const open = document.querySelector("#abrir")
+const close = document.querySelector("#cerrar")
+
+open.addEventListener("click", ()=>{
+	nav.classList.add("visible")
+})
+
+
+close.addEventListener("click", ()=>{
+	nav.classList.remove("visible")
+})
+
+
+const enlaces = document.querySelectorAll("#link")
+
+enlaces.forEach((enlace) => {
+	enlace.addEventListener("click", () => {
+	  nav.classList.remove("visible");
+	});
+  });
+
+console.log(enlaces);
+
+
+
+
+
+
+
+
+
+
+
+/*hero animación letras */
+
 const typed = new Typed(`.typed`, {
     strings: [
        `<i class="saludo">Emiliano Zapata</i>` ,
